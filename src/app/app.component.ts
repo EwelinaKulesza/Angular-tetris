4e1ac791eb92f8575cfc9759ac36a8f74bf0bc16
EwelinaKulesza <cegla00@gmail.com>
2021-02-15 22:45:05 +0100

    12ZliczPkty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TetrisKewelina';
+  public title = 'TetrisKewelina';
+  public score = 0;
+
+  public onLineCleared() {
+    let someValue: number;
+    this.score++;
+    someValue = this.score;
+    return someValue.toString();
+  }
 }
