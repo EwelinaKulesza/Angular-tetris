1ebeb2f2578e2104ca57561b6b67e5e1d9b151a5
EwelinaKulesza <cegla00@gmail.com>
2021-02-15 21:14:10 +0100

    jedenascie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestComponentComponent } from './test-component/test-component.component';
-import { C1Component } from './testModule/c1/c1.component';
-import { MyComponentComponent } from './my-component/my-component.component';
-import { MyComponent2Component } from './my-component2/my-component2.component';
-import { MyComponent3Component } from './my-component3/my-component3.component';
-import { MyPipePipe } from './my-pipe.pipe';
-import { MyPipe2Pipe } from './my-pipe2.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponentComponent,
-    C1Component,
-    MyComponentComponent,
-    MyComponent2Component,
-    MyComponent3Component,
-    MyPipePipe,
-    MyPipe2Pipe
   ],
   imports: [
     BrowserModule
--- src/app/my-pipe2.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MyPipe2Pipe } from './my-pipe2.pipe';
-
-describe('MyPipe2Pipe', () => {
-  it('create an instance', () => {
-    const pipe = new MyPipe2Pipe();
-    expect(pipe).toBeTruthy();
-  });
-});
