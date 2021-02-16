9d4a98d9325a22c00a46ca42437009054b3b6075
EwelinaKulesza <cegla00@gmail.com>
2021-02-16 20:27:59 +0100

    trzynascie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {TetrisCoreModule} from 'ngx-tetris';
+import { IntroPageComponent } from './intro-page/intro-page.component';
+import { GamePageComponent } from './game-page/game-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    IntroPageComponent,
+    GamePageComponent,
   ],
   imports: [
     BrowserModule,
