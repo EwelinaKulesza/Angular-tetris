97530322b952c1522157f7d6e365caae8e973ae6
EwelinaKulesza <cegla00@gmail.com>
2021-02-18 15:46:54 +0100

    osiemnacie

--- src/app/intro-page/intro-page.component.ts
+++ src/app/intro-page/intro-page.component.ts
@@ -6,26 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro-page.component.css']
 })
 export class IntroPageComponent implements OnInit {
-  public nameValue: string = '...';
-  private emailValue: string = '';
-  public formNameIsShows: Boolean = true;
-  public formEmailIsShows: Boolean = true;
+  public nameValue: string = '..';
+  public emailValue: string = '';
+  public validNameMessage: string;
+  public validEmailMessage: string;
+  public nameIsInvalid: Boolean = true;
+  public emailIsInvalid: Boolean = true;
 
   onNameEnter(value: string) {
-    this.nameValue = value + "!";
-    this.formNameIsShows = false;
+    let mabyNameTemp: string = value;
+    if (mabyNameTemp.length > 3) {
+      this.nameIsInvalid = false;
+      this.validNameMessage = "";
+      this.nameValue = value + "!";
+    } else if (mabyNameTemp.length <= 3 ) {
+    this.validNameMessage = "Dłuższe imiona mają ludzie z gazet: Olek O., Anna P.";
+    } else {"Z Twoim imeniem jest coś nie tak";
+          }
   }
 
   onEmailEnter(value: string) {
     let mabyEmailTemp: string = value;
-    //prosty walidator czy mail, jak będe miała czas to zerknę na regex'y albo jaies gotowe walidatory Angulara
-    if (mabyEmailTemp.includes('@') && mabyEmailTemp.includes('.') && (mabyEmailTemp.length > 7)) {
+    //prosty walidator czy mail czy byle co, jak będe miała czas to zerknę na regex'y albo jaies gotowe walidatory Angulara
+    if (mabyEmailTemp.includes('@') && mabyEmailTemp.includes('.') && (mabyEmailTemp.length > 6)) {
+      this.emailIsInvalid = false;
+      this.validEmailMessage = "";
       this.emailValue = value;
-      this.formEmailIsShows = false;
-    } 
-    
-
+    } else {
+      "Coś jest nie nie tak z tym mailem."}
   }
+
+  checkIsValidForms() {
+    this.validNameMessage = "";
+    if (this.emailIsInvalid) this.validEmailMessage = "Ten email nie wygląda na prawidłowy."
+    if (this.nameIsInvalid) this.validNameMessage = "Coś nie tak z Twoim imieniem."
+    }
 
 
   constructor() { }
