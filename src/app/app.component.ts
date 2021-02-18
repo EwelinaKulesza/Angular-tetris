3f2956a84632c5956bc713453980c2fca99dfc1c
EwelinaKulesza <cegla00@gmail.com>
2021-02-18 15:56:43 +0100

    dwadziescia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tetris by K. Ewelina';
-  //public startTime = 0;
-  // find strat time in seconds
-  public startTime = 60*60*(new Date().getHours()) + 60*(new Date().getMinutes()) + new Date().getSeconds()
   public CurrentTime: any;
+  public firstPageOnTop: Boolean = true;
   constructor() {
     setInterval(() => {
       this.CurrentTime = new Date().getHours() + ':' + new Date().getMinutes() + ':'+  new Date().getSeconds()}, 1);
