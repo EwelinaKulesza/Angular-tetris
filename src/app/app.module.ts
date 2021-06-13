161168e0f55f6f4d5fae25a7cc8da44ac4dda6c7
EwelinaKulesza <cegla00@gmail.com>
2021-06-13 20:38:16 +0200

    dodano komponent high scores, który po kliknięciu pobiera dane punktów z backendu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, Output, EventEmitter } from '@angular/core';
+import { NgModule, Output, EventEmitter, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TetrisCoreModule } from 'ngx-tetris';
@@ -8,23 +8,28 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { GamePageModule } from './game-page/game-page.module';
 import { IntroPageModule } from './intro-page/intro-page.module';
+// import { HighScoreModule } from './intro-page/high-score/high-score.module';
+import { HighScoreComponent } from './intro-page/high-score/high-score.component';
+import { HighScoreService } from './high-score.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     // IntroPageComponent,
     // GamePageComponent,
+    // HighScoreComponent
 
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
-    // TetrisCoreModule,
-    // ReactiveFormsModule,
     IntroPageModule,
-    GamePageModule
+    GamePageModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [HttpClient, HighScoreService],
+  bootstrap: [AppComponent],
+  // schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
