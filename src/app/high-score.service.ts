3f9ffb8384d3514688391a942b1d5390dcc8898a
EwelinaKulesza <cegla00@gmail.com>
2021-06-15 20:21:20 +0200

    old way high score

--- src/app/high-score.service.ts
+++ src/app/high-score.service.ts
@@ -12,15 +12,15 @@ export class HighScoreService {
     const headers = new HttpHeaders({ "Content-Type": "application/json" });
     // const headers = new HttpHeaders({ "Content-Type": "text/plain" });
 
-
     return this.httpClient.get<any>('https://tetris.chrum.it/scores', { headers });
   }
 
-  // postScores(bbb) {
-  //   return this.httpClient.post<any>('/scores', null, {
-  //     params: {
-  //       aaa: bbb,
-  //     },
-  //   });
-  // }
+  postScores(scores: { name: string, score: string }) {
+    return this.httpClient.post<any>('https://tetris.chrum.it/scores', null, {
+      params: {
+        "name": scores.name,
+        "score": scores.score
+      }
+    });
+  }
 }
